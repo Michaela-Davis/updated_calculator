81bedc34628b77cebdc8aa719473cb4ea28cfebc
Joe Karasek <joseph.karasek@gmail.com>
2017-01-11 20:34:03 -0800

    add logic for addition form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
+
+  // Add Form
+  // use jQuery to grab the addForm and attach submit event
   $("#addForm").submit(function(event) {
-    console.log("hi");
+    // Collect input from form
+    var inputOne = $("#addInputOne").val();
+    var inputTwo = $("#addInputTwo").val();
+
+    // Add inputs together
+    // Declaring a third variable called result
+    var result = parseInt(inputOne) + parseInt(inputTwo);
+
+    // Use jQuery to display result
+    $("#addOutput").text(result);
 
     event.preventDefault();
   });
