1323509245999e4912afe9fe2409a29f132c23fc
Joe Karasek <joseph.karasek@gmail.com>
2017-01-11 20:43:11 -0800

    add logic for subtraction form

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,20 @@ $(document).ready(function() {
   });
 
   // Subraction Form
+  // use jQuery to grab the subtractForm and attach submit event
+  $("#subtractForm").submit(function(event) {
+    // Collect input from form
+    var inputOne = $("#subtractInputOne").val();
+    var inputTwo = $("#subtractInputTwo").val();
 
+    // Add inputs together
+    // Declaring a third variable called result
+    var result = parseInt(inputOne) - parseInt(inputTwo);
+
+    // Use jQuery to display result
+    $("#subtractOutput").text(result);
+
+    event.preventDefault();
+  });
 
 });
